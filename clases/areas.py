c439fa1f2a6fa8dad619def1f6c621c755a9e036
paulina-reyes <193105-9@iberoleon.edu.mx>
2025-09-18 12:02:45 -0600

    Agregado circulo

--- clases/areas.py
+++ clases/areas.py
@@ -3,11 +3,16 @@ class CalculadoraAreas:
 
     def _leer_datos(self, figura):
 
+        if figura == 'círculo':
+            radio = float(input(f"Introduce la base del {figura}: "))
+            return radio
+
         base = float(input(f"Introduce la base del {figura}: "))
         altura = float(input(f"Introduce la altura del {figura}: "))
 
         return base, altura
-
+    
+    
     def _mostrar_resultado(self, figura, area):
       
         print(f"El área del {figura} es: {area}")
@@ -25,3 +30,11 @@ class CalculadoraAreas:
 
         area = (base * altura) / 2
         self._mostrar_resultado("triángulo", area)
+
+    def calcular_area_circulo(self):
+        import numpy as np
+
+        radio = self._leer_datos("círculo")
+        
+        area = radio**2 * np.pi
+        self._mostrar_resultado("círculo", area)
